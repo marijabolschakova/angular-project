51bf0dfa4a81d388f21fdc70d994a6ca57697198
Masha <marija.bolschakova@yandex.ru>
2023-04-20 22:03:41 +0200

    made up image pipe

--- src/app/core/pipe/image-source.pipe.ts
+++ src/app/core/pipe/image-source.pipe.ts
@@ -1,13 +1,11 @@
 import {Pipe, PipeTransform} from "@angular/core";
-import {Thumbnail} from "../services/tmdb.service";
-
 
 @Pipe({
   name: 'imageSource',
 })
 
 export class ImageSourcePipe implements PipeTransform {
-  transform(value: Thumbnail, size = ''): string {
-    return `${value.path}/${size}.${value.extension}`;
+  public transform(size: string, value: string | undefined): string {
+    return `https://image.tmdb.org/t/p/${size}${value}`;
   }
 }
